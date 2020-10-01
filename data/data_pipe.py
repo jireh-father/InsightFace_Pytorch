a264160f1be5f893385fe7d301e19f96b5a4d3bf
seoilgun <seoilgun@gmail.com>
2020-10-02 00:19:41 +0900

    implement font train

--- data/data_pipe.py
+++ data/data_pipe.py
@@ -150,7 +150,8 @@ def get_common_val_data(data_path, max_positive_cnt, batch_size, pin_memory, num
                 pass
         max_positive_cnt = len(positive_files)
         issame += [True] * int(len(positive_files) / 2)
-
+    print(max_positive_cnt)
+    print(issame)
     negative_files = []
     if use_neg:
         total_negative_cnt = 0
@@ -171,7 +172,8 @@ def get_common_val_data(data_path, max_positive_cnt, batch_size, pin_memory, num
             negative_files.append(label_files_list[neg_label_idx][neg_item_idx])
             total_negative_cnt += 1
         issame += [False] * int(len(negative_files) / 2)
-
+    print(negative_files)
+    print(issame)
     loader = DataLoader(ValDataset(positive_files + negative_files, val_transforms), batch_size=batch_size,
                         shuffle=False, pin_memory=pin_memory,
                         num_workers=num_workers)
