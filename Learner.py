911009161988e6bc8c357713073a4479a73bea64
seoilgun <seoilgun@gmail.com>
2020-10-05 14:06:45 +0900

    implement font train

--- Learner.py
+++ Learner.py
@@ -59,7 +59,8 @@ class face_learner(object):
             if not inference:
                 self.milestones = conf.milestones
                 if train_loader is None:
-                    self.loader, self.class_num = get_train_loader(conf, train_transforms)
+                    if conf.train:
+                        self.loader, self.class_num = get_train_loader(conf, train_transforms)
                 else:
                     self.loader = train_loader
                     self.class_num = conf.num_classes
